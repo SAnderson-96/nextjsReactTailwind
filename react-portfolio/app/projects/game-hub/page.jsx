72d078377e266252d95b1971972c82df12eb75af
SAnderson-96 <78217771+SAnderson-96@users.noreply.github.com>
2023-05-22 14:28:54 -0400

    added new project

---
+++ react-portfolio/app/projects/game-hub/page.jsx
@@ -0,0 +1,25 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import gameHub from "../../../public/game-hub/game-hub.png";
+import { Box } from "@mui/material";
+import { Text } from "@nextui-org/react";
+import Technologies from "@/components/technologies";
+
+export default function GameHub(){
+    const data = require('../../../public/game-hub/data.json');
+    return (
+      <Box marginX={16}>
+        <Box margin='auto' textAlign='center' marginY={2}>
+          <Text size={24} color='white'>
+            Click on Image to go to Website.
+          </Text>
+        </Box>
+        <Link href='https://sanderson-game-hub.vercel.app/' target='_blank'>
+          <Image src={gameHub} />
+        </Link>
+
+        <Technologies className='mx-auto' technologies={data.technologies} />
+      </Box>
+    );
+}
